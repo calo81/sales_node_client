dfc5143a6adb7846ef9135f74d33d1977b45b529
carlo scarioni <carlo.scarioni@gmail.com>
2014-05-11 20:10:46 +0100

    removed unused code

--- server.js
+++ server.js
@@ -7,19 +7,6 @@ var http = require('http')
  , cache = {}
 
 server.use(restify.bodyParser()); 
-function send404(response) {
-  response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: resource not found.');
-  response.end();
-}
-
-function sendFile(response, filePath, fileContents) {
-  response.writeHead(
-    200,
-    {"content-type": mime.lookup(path.basename(filePath))}
-  );
-  response.end(fileContents);
-}
 
 function saleReceived(person, amount, policy){
 	if(taskBoardServer.sales[person]){
@@ -32,27 +19,6 @@ function saleReceived(person, amount, policy){
 	}
     taskBoardServer.sockets[0].broadcast.emit('sale_broadcast', {id: policy, person: person, quantity: taskBoardServer.sales[person]["quantity"], amount: taskBoardServer.sales[person]["amount"]});
   	
-}
-
-function serveStatic(response, cache, absPath) {
-  if (cache[absPath]) {
-    sendFile(response, absPath, cache[absPath]);
-  } else {
-    fs.exists(absPath, function(exists) {
-      if (exists) {
-        fs.readFile(absPath, function(err, data) {
-          if (err) {
-            send404(response);
-          } else {
-            cache[absPath] = data;
-            sendFile(response, absPath, data);
-          }
-        });
-      } else {
-        send404(response);
-      }
-    });
-  }
 }
 
 server.post('/sale', function(req, res, next){
